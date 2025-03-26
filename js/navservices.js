49c76aa3c2b021fb254ac4c559cbff8bdb9d9701
Yousef Ahmed <floky4444@gmail.com>
2025-03-26 22:36:07 +0200

    main

--- js/navservices.js
+++ js/navservices.js
@@ -1,99 +1,91 @@
 document.querySelector('nav').innerHTML = `
-<div class="box f-s">
-        <a href="../index.html" class="logo">
-          <img src="../media/logo.png" alt="">
-        </a>
-        <div class="links">
-          <div class="ls">
-            <ul class="mainLinks">
-              <li><a class="link" href="../index.html">Home</a></li>
-              <li><a class="link" href="../about.html"> About Us</a></li>
-              <li class="drop">
-                <a class="link active" href="javascript:;">Services <i class="fa-regular fa-angle-down"></i></a>
-                <div class="dropMenu">
-                  <div class="box dropItems">
-                    <a href="./7.html">
-                    <!-- <i class="fa-regular fa-pen"></i> Translation -->
-                    <img style="margin-top: 3px;" src="../media/icons/trans2.svg" alt="">  Translation
+    <div class="box f-s">
+      <a href="../index.html" class="logo">
+        <img src="../media/logo.png" alt="">
+      </a>
+      <div class="links">
+        <div class="ls">
+          <ul class="mainLinks">
+            <li><a class="link" href="../index.html">Home</a></li>
+            <li><a class="link" href="../about.html"> About Us</a></li>
+            <li class="drop">
+              <a class="link active" href="javascript:;">Services <i class="fa-light fa-angle-down"></i></a>
+              <div class="dropMenu">
+                <div class="box dropItems">
+                  <a href="./translation.html">
+                    <i class="fa-light fa-language"></i> Translation
                   </a>
-                  <a href="./2.html">
-                    <i>
-                      <img  style="margin-top: 3px;" src="../media/icons/trasnslation2.svg" alt="Localization Icon">
-                    </i> Localization
+                  <a href="./localization.html">
+                    <i class="fa-light fa-globe-americas"></i> Localization
                   </a>
-                    <a href="./1.html">
-                      <i class="fa-regular fa-desktop"></i> Desktop Publishing Services
-                    </a>
-                    <a href="./3.html">
-                      <i>
-                        <img src="../media/icons/MTPE.svg" alt="MTPE Icon">
-                      </i>Machine Translation <br> Post-Editing (MTPE)
-                    </a>
-                    <a href="./4.html">
-                      <i class="fa-regular fa-book"></i> Terminology Management
-                    </a>
-                    <a href="./5.html">
-                      <i class="fa-regular fa-lightbulb-on"></i> Transcreation
-                    </a>
-                    <a href="./6.html">
-                      <i class="fa-regular fa-keyboard"></i> Transcription
-                    </a>
-                  </div>
+                  <a href="./multimedia.html">
+                    <i class="fa-light fa-photo-film-music"></i> Multimedia
+                  </a>
+                  <a href="./interpretation.html">
+                    <i class="fa-light fa-head-side-gear"></i> Interpretation
+                  </a>
+                  <a href="./transcreation.html">
+                    <i class="fa-light fa-lightbulb-on"></i> Transcreation
+                  </a>
+                  <a href="./mtpe.html">
+                    <i class="fa-light fa-user-robot"></i> MTPE
+                  </a>
                 </div>
-  
-              </li>
-              <li class="drop">
-                <a class="link" href="javascript:;"> Industries <i class="fa-regular fa-angle-down"></i></a>
-                <div class="dropMenu">
-                  <div class="box dropItems">
-                    <a href="../industries/11.html">
-                    <i class="fa-regular fa-cogs"></i> Technology
-                    </a>
-                    <a href="../industries/1.html">
-                      <i class="fa-regular fa-car"></i> Automotive
-                    </a>
-                    <a href="../industries/2.html">
-                      <i class="fa-regular fa-shopping-cart"></i> E-commerce
-                    </a>
-                    <a href="../industries/3.html">
-                      <i class="fa-regular fa-book-open"></i> Education & E-Learning
-                    </a>
-                    <a href="../industries/4.html">
-                      <i class="fa-regular fa-coins"></i> Financial
-                    </a>
-                    <a href="../industries/5.html">
-                      <i class="fa-regular fa-gamepad"></i> Games 
-                    </a>
-                    <a href="../industries/6.html">
-                      <i class="fa-regular fa-balance-scale"></i> Legal
-                    </a>
-                    <a href="../industries/7.html">
-                      <i class="fa-regular fa-heartbeat"></i> Life Sciences
-                    </a>
-                    <a href="../industries/8.html">
-                      <i class="fa-regular fa-bullhorn"></i> Marketing
-                    </a>
-                    <a href="../industries/9.html">
-                      <i class="fa-regular fa-user-md"></i> Medical
-                    </a>
-                    <a href="../industries/10.html">
-                      <i class="fa-regular fa-pills"></i> Pharma Translation Services
-                    </a>
-                  </div>
-                
+              </div>
+
+            </li>
+            <li class="drop">
+              <a class="link" href="javascript:;"> Industries <i class="fa-light fa-angle-down"></i></a>
+              <div class="dropMenu">
+                <div class="box dropItems">
+                  <a href="../industries/technology.html">
+                    <i class="fa-light fa-cogs"></i> Technology
+                  </a>
+                  <a href="../industries/automotive.html">
+                    <i class="fa-light fa-car"></i> Automotive
+                  </a>
+                  <a href="../industries/e-commerce.html">
+                    <i class="fa-light fa-shopping-cart"></i> Ecommerce & Retail
+                  </a>
+                  <a href="../industries/education-e-learning.html">
+                    <i class="fa-light fa-book-open"></i> Education & E-Learning
+                  </a>
+                  <a href="../industries/financial.html">
+                    <i class="fa-light fa-coins"></i>Banking & Financial
+                  </a>
+                  <a href="../industries/games.html">
+                    <i class="fa-light fa-gamepad"></i> Games
+                  </a>
+                  <a href="../industries/legal.html">
+                    <i class="fa-light fa-balance-scale"></i> Legal
+                  </a>
+                  <a href="../industries/life-sciences.html">
+                    <i class="fa-light fa-heartbeat"></i> Life Sciences
+                  </a>
+                  <a href="../industries/marketing.html">
+                    <i class="fa-light fa-bullhorn"></i> Marketing & Advertising
+                  </a>
+                  <a href="../industries/travel-hospitality.html">
+                    <i class="fa-light fa-plane-departure"></i> Travel & Hospitality
+                  </a>
+                  <a href="../industries/manufacturing-engineering.html">
+                    <i class="fa-light fa-industry"></i> Manufacturing & Engineering
+                  </a>                  
                 </div>
-  
-              </li>
-              <li><a class="link" href="../contact.html"> Contact Us</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="last">
-          <button class="menu">
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
+
+              </div>
+
+            </li>
+            <li><a class="link" href="../contact.html"> Contact Us</a></li>
+          </ul>
         </div>
       </div>
+      <div class="last">
+        <button class="menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+    </div>
 `
---
+++ js/sfooter.js
@@ -0,0 +1,65 @@
+document.querySelector('footer').innerHTML = `
+<footer>
+      <div class="box footer">
+        <div class="footerLogo">
+          <img src="../media/footerLogo.png" alt="">
+          <p>
+            Based in Egypt, the vibrant hub of the Middle East, TRANSLIE has established itself as a premier localization
+            company, specializing in delivering high-quality, professional translation services to clients across the
+            Middle East and Africa.
+  
+          </p>
+          <p>
+  
+            Our team consists of certified and experienced native translators, equipped with the expertise and
+            knowledge to meet the unique demands and requirements of even the most discerning clients.
+          </p>
+        </div>
+        <div class="footerLinkGroup">
+          <div class="linkGroup">
+            <h2> Services</h2>
+            <ul>
+              <li><a href="./translation.html">
+                  <img src="../media/icons/footerLink.svg" alt=""> Translation
+                </a></li>
+              <li><a href="./localization.html">
+                  <img src="../media/icons/footerLink.svg" alt=""> Localization
+                </a></li>
+              <li><a href="./multimedia.html">
+                  <img src="../media/icons/footerLink.svg" alt=""> Multimedia
+                </a></li>
+              <li><a href="./interpretation.html">
+                  <img src="../media/icons/footerLink.svg" alt=""> Interpretation
+                </a></li>
+              <li><a href="./transcreation.html">
+                  <img src="../media/icons/footerLink.svg" alt=""> Transcreation
+                </a></li>
+              <li><a href="./mtpe.html">
+                  <img src="../media/icons/footerLink.svg" alt=""> MTPE
+                </a></li>
+            </ul>
+          </div>
+          <div class="linkGroup cont">
+            <h2> Contact Us</h2>
+            <ul>
+              <li><a href="tel:+20100xxxxxxx" target="_blank"><img src="../media/icons/call.png" alt=""> +20100xxxxxxx </a>
+              </li>
+              <li><a href="mailto:info@TRANSLIE.com" target="_blank"><img src="../media/icons/mail.png" alt="">
+                  info@TRANSLIE.com</a></li>
+            </ul>
+            <div class="media">
+              <a href="##" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
+              <a href="##" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+              <a href="##"><i class="fa-brands fa-instagram"></i></a>
+              <a href="##"><i class="fa-brands fa-whatsapp"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="box copyInner">
+        <div class="copy">
+          Copyright © 2022 <strong>TRANSLIE</strong> | All rights reserved.
+        </div>
+      </div>
+    </footer>
+`;
